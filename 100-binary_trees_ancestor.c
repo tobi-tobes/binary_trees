a49f0a0e7fe7637a68b79833d2fa6b98e8ca24d2
Tobi Tijani <tobi_tijani@yahoo.com>
2023-05-30 15:33:37 -0700

    Task 19 correction

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,6 +1,29 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: pointer to the root node to measure the depth
+ *
+ * Return: the depth of the tree or 0 if tree is NULL
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+	const binary_tree_t *temp;
+
+	if (tree == NULL)
+		return (0);
+
+	temp = tree;
+	while (temp->parent != NULL)
+	{
+		temp = temp->parent;
+		depth++;
+	}
+	return (depth);
+}
+
+/**
  * populate - fills an array with pointers to ancestors
  * @tree: pointer to the node to retrieve ancestors
  * @array: array to populate
