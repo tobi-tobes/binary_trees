3df36f0763fbc92a52dce1d3371be9bb2f00fd01
Tobi Tijani <tobi_tijani@yahoo.com>
2023-06-01 03:50:23 -0700

    Task 28 fix: added duplicate function

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -19,6 +19,54 @@ bst_t *find_min(bst_t *root)
 }
 
 /**
+ * binary_tree_node - creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to put in the new node
+ *
+ * Return: pointer to new node or NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	return (new_node);
+}
+
+/**
+ * duplicate - creates a copy of a node in a binary search tree
+ * @root: pointer to the root node of the tree where you will remove a node
+ * @min: inorder successor of the node to be deleted
+ *
+ * Return: the new root node of the tree after removing the desired value
+ */
+bst_t *duplicate(bst_t *root, bst_t *min)
+{
+	bst_t *temp;
+
+	temp = binary_tree_node(NULL, min->n);
+	temp->parent = root->parent;
+	temp->left = root->left;
+	temp->right = root->right;
+
+	if (root->left != NULL)
+		root->left->parent = temp;
+
+	if (root->right != NULL)
+		root->right->parent = temp;
+
+	free(root);
+	return (temp);
+}
+
+/**
  * bst_remove - removes a node from a Binary Search Tree
  * @root: pointer to the root node of the tree where you will remove a node
  * @value: value to remove in the tree
@@ -58,7 +106,7 @@ bst_t *bst_remove(bst_t *root, int value)
 		else
 		{
 			temp = find_min(root->right);
-			root->n = temp->n;
+			root = duplicate(root, temp);
 			root->right = bst_remove(root->right, temp->n);
 		}
 	}
--- binary_trees.h
+++ binary_trees.h
@@ -70,6 +70,7 @@ bst_t *bst_insert(bst_t **tree, int value);
 bst_t *array_to_bst(int *array, size_t size);
 bst_t *bst_search(const bst_t *tree, int value);
 bst_t *find_min(bst_t *root);
+bst_t *duplicate(bst_t *root, bst_t *min);
 bst_t *bst_remove(bst_t *root, int value);
 
 #endif
